c1c1bd8208f1c9adabdaaca90de3f27552dac82d
JaValderaContador <javier1gdvc@gmail.com>
2024-03-25 13:54:50 -0300

    Home page done

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
   get '/home', to: 'static_pages#home'
 end
