90e2c61af8a629aa954478d5000f95075b3561ff
JaValderaContador <javier1gdvc@gmail.com>
2024-03-27 16:54:15 -0300

    submit button working v2

--- app/javascript/contact_form.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        document.getElementById('confirmationMsg').style.display = 'block';
-        
-        this.reset();
-    });
-});
